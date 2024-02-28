3c7a77674cc06ef192259665df2d98177a24337c
Eric Harris-Braun <eric@harris-braun.com>
2024-02-28 11:30:46 -0500

    Update to new syn version

--- ui/src/AboutDialog.svelte
+++ ui/src/AboutDialog.svelte
@@ -72,7 +72,7 @@
 </script>
 
 
-<sl-dialog label="Griffy!: UI v0.1.0 for DNA v0.1.0" bind:this={dialog} width={600} >
+<sl-dialog label="Griffy!: UI v0.2.0 for DNA v0.2.0" bind:this={dialog} width={600} >
     <div class="about">
         <p>Griffy! is a demonstration Holochain app built by Lightning Rod Labs.</p>
         <p> <b>Developers:</b>
